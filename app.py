9285e5a94ad9b6c5fe7bf5b10ac3b88508417367
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2018-10-24 00:50:23 +0900

    api_key 값을 header를 통해 얻게 변경

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ import os
 from mailslurp_client import MailSlurpClient
 from flask_cors import CORS
 
-mail_api = MailSlurpClient(os.environ['API_KEY'])
-
 app = Flask(__name__)
 CORS(app)
 
@@ -16,11 +14,17 @@ def index():
 
 @app.route("/inboxes", methods=['GET'])
 def inboxes():
+    api_key = request.headers['API_KEY']
+    mail_api = MailSlurpClient(api_key=api_key)
+
     return jsonify(mail_api.get_inboxes())
 
 
 @app.route("/inboxes/<inbox_id>/messages", methods=['GET'])
 def message(inbox_id):
+    api_key = request.headers['API_KEY']
+    mail_api = MailSlurpClient(api_key=api_key)
+
     messages = mail_api.get_messages(inbox_id)
     for msg in messages:
         m = mail_api.get_eamil_text_contents(msg['body'])
